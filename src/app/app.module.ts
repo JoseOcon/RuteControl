c8f6e9d6e9cc200dbe3228137df482e63d16897f
Jose Ocon <josebermudez587@gmail.com>
2021-01-24 15:56:02 -0600

    Instalacion de geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from "@angular/router";
 import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
@@ -27,6 +28,7 @@ import { ComponentsModule } from "./components/components.module";
     ComponentsModule
   ],
   providers: [
+    Geolocation,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
