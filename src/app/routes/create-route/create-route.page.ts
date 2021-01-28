2c3887c50a73c3936ff4f06bf9383f82ab89bb55
Jose Ocon <josebermudez587@gmail.com>
2021-01-28 00:11:04 -0600

    Se pinta la ruta

--- src/app/routes/create-route/create-route.page.ts
+++ src/app/routes/create-route/create-route.page.ts
@@ -13,9 +13,11 @@ declare var google;
 })
 export class CreateRoutePage implements OnInit {
   mapElem: HTMLElement = null;
+  indicatorsEle: HTMLElement = null;
   myMap = null;
   myLatLng = null;
   loading = null;
+  showInfo: boolean = false;
 
   markers: Array<any> = [];
 
@@ -23,6 +25,9 @@ export class CreateRoutePage implements OnInit {
     url: "../../../assets/imgs/car-icon.png",
     scaledSize: new google.maps.Size(50, 60),
   };
+
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
 
   constructor(
     private geolocation: Geolocation,
@@ -52,10 +57,13 @@ export class CreateRoutePage implements OnInit {
 
   createMap() {
     this.mapElem = document.getElementById("map");
+    this.indicatorsEle = document.getElementById("indicators");
     this.myMap = new google.maps.Map(this.mapElem, {
       center: this.myLatLng,
       zoom: 10,
     });
+    this.directionsDisplay.setMap(this.myMap);
+    this.directionsDisplay.setPanel(this.indicatorsEle);
   }
 
   addMapEvents() {
@@ -68,14 +76,14 @@ export class CreateRoutePage implements OnInit {
         let cstmLatLg = mapsMouseEvent.latLng.toJSON();
         let position = {
           lat: cstmLatLg.lat,
-          lng: cstmLatLg.lng
-        }
-        this.addMarker(position)
+          lng: cstmLatLg.lng,
+        };
+        this.addMarker(position);
       });
     });
   }
 
-  addMarker(position){
+  addMarker(position) {
     let myMarker = new google.maps.Marker({
       position: position,
       map: this.myMap,
@@ -84,10 +92,57 @@ export class CreateRoutePage implements OnInit {
 
     //Delete event
     myMarker.addListener("click", () => {
+      this.markers = this.markers.filter(
+        (elem) =>
+          elem.position.toJSON().lat != myMarker.position.toJSON().lat &&
+          elem.position.toJSON().lng != myMarker.position.toJSON().lng
+      );
       myMarker.setMap(null);
+      this.calculateRute();
     });
 
     this.markers.push(myMarker);
+    this.calculateRute();
   }
 
+  async calculateRute() {
+    if (this.markers.length > 2) {
+
+      let maxIndex = this.markers.length - 1;
+      let wayPoints = await this.getWayPoints();
+      this.directionsService.route(
+        {
+          origin: this.markers[0].position.toJSON(),
+          destination: this.markers[maxIndex].position.toJSON(),
+          waypoints: wayPoints,
+          optimizeWaypoints: true,
+          travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (response, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            this.directionsDisplay.setDirections(response);
+          } else {
+            alert("Could not display directions due to: " + status);
+          }
+        }
+      );
+    }
+  }
+
+  getWayPoints() {
+    let wayPoints = [];
+
+    for (let i = 0; i < this.markers.length; i++) {
+      if (i > 0 && i < this.markers.length - 1) {
+        wayPoints.push({
+          location: {
+            lat: this.markers[i].position.toJSON().lat,
+            lng: this.markers[i].position.toJSON().lng,
+          },
+          stopover: true,
+        });
+      }
+    }
+    return wayPoints;
+  }
 }
